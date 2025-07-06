22771fd35626cc7824169155024bd6a6be8f90ff
stevenmiranda0 <00177125@uca.edu.sv>
2025-07-06 01:01:00 -0600

    roulette

--- src/roulette.h
+++ src/roulette.h
@@ -19,3 +19,47 @@ void playRoulette(Player &player);
 
 #endif
 
+include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+// Player structure
+struct Player {
+    string name;
+    int money;
+    int wins;
+};
+
+
+// Show betting menu
+void showMenu() {
+    cout << "\n--- BETTING OPTIONS ---\n";
+    cout << "1. Exact number (0-36)\n";
+    cout << "2. Color (red or black)\n";
+    cout << "3. Even or odd\n";
+    cout << "Choose an option: ";
+}
+
+
+// Get the color of the number
+string getColor(int number) {
+    if (number == 0) return "green";
+    return (number % 2 == 0) ? "black" : "red";
+}
+
+// Validate bet amount
+int validateBet(int availableMoney) {
+    int bet;
+    do {
+        cout << "\nEnter your bet amount (minimum $1): ";
+        cin >> bet;
+        if (bet < 1 || bet > availableMoney) {
+            cout << "Invalid bet. Please try again.\n";
+        }
+    } while (bet < 1 || bet > availableMoney);
+    return bet;
+}
