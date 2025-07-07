f63b1e31b53e72efb734db999974d2166f7717c1
RosGuadalupeEscobar <00235325@uca.edu.sv>
2025-07-07 14:16:21 -0600

    Repartir las cartas

--- src/Poker.h
+++ src/Poker.h
@@ -1,16 +1,27 @@
+//CAMBIAR COMENTARIOS A INGLES
+
 #ifndef Poker_H 
-#define Poker_H
+#define Poker_H 
+//Estos dos son directivas para evitar incluir el archivo .h mas de una vez
 
 #include <iostream>
-#include <iomanip>
-#include <string>
-#include <fstream>
+#include <iomanip> 
+//Se utiliza para controlar como se muestran los datos en pantalla, Se utilizara para alinear los numeros de las cartas
+#include <string> 
+//Para utilizar datos tipo string
+#include <fstream> 
+//para uso de archivos
+#include <cstdlib>  
+// Para rand() y srand() que sera el random para barajear y repartir cartas
+#include <ctime>    
+// Inicializa el random con un valor diferente cada vez
+
 using namespace std;
 
 // Estructura Carta
 struct Carta
 {
-    int numero;
+    string numero;
     string palo;
     string color;
 };
@@ -31,10 +42,10 @@ void imprimirCartaLinea(const Carta &carta, int linea)
     else
         paloSimbolo = "?";
 
-    string colorAnsi = (carta.color == "rojo") ? "\033[31m" : "\033[30m";
+    string colorAnsi = (carta.color == "rosado") ? "\033[31m" : "\033[30m"; //cambio de color a las cartas
     string resetAnsi = "\033[0m";
 
-    //diseño de las cartas
+    //Diseño de las cartas
     switch (linea)
     {
     case 0:
@@ -61,7 +72,7 @@ void imprimirCartaLinea(const Carta &carta, int linea)
     }
 }
 
-//array donde se guarda el mazo de 52 cartas que se exportan desde el archivo .txt
+//Array donde se guarda el mazo de 52 cartas que se exportan desde el archivo .txt
 Carta mazo[52];
 string formatearCarta(const Carta &carta);
 
@@ -100,25 +111,48 @@ void imprimirManoHorizontal(const Carta mano[], int numCartas)
 }
 
 
+// Declaración de las manos por cada jugador
+Carta manoJugador1[5];
+Carta manoJugador2[5];
 
+// Arreglo auxiliar para marcar qué cartas del mazo ya han sido repartidas
+bool cartaUsada[52] = {false};
 
+// Función que reparte 5 cartas aleatorias a cada jugador sin repetir
+void repartirCartas()
+{
+    srand(time(0)); // Inicializa la semilla para números aleatorios (una sola vez)
 
+    int cartasRepartidas = 0;
 
+    // Repartir 5 cartas al jugador 1
+    for (int i = 0; i < 5; i++)
+    {
+        int indice;
+        do
+        {
+            indice = rand() % 52; // Genera un número aleatorio entre 0 y 51
+        } while (cartaUsada[indice]); // Repite mientras la carta ya haya sido usada
 
+        manoJugador1[i] = mazo[indice]; // Asigna la carta al jugador
+        cartaUsada[indice] = true;      // Marca la carta como usada
+        cartasRepartidas++;
+    }
 
+    // Repartir 5 cartas al jugador 2
+    for (int i = 0; i < 5; i++)
+    {
+        int indice;
+        do
+        {
+            indice = rand() % 52;
+        } while (cartaUsada[indice]);
 
-
-
-
-
-
-
-
-
-
-
-
-
+        manoJugador2[i] = mazo[indice];
+        cartaUsada[indice] = true;
+        cartasRepartidas++;
+    }
+}
 
 
 #endif
